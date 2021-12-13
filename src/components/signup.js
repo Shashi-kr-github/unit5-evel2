46694082f1a66f74ca1b72bae49a0606105e04dc
Shashijass <shashi16ic14@gmail.com>
2021-12-13 15:57:49 +0530

    3rd commit

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,14 +1,20 @@
-import {Grid, Paper} from '@mui/material/core'
+import { Avatar, Grid, Paper, Typography } from "@mui/material/";
 
 const Signup = () => {
-    return (
+  const paperStyle = { padding: "30px 20px", width: 300, margin: "20px auto" };
+  return (
     <Grid>
-      <Paper>
-        <h2>singup</h2>
+      <Paper elevation={20} style={paperStyle}>
+        <Grid>
+          <Avatar></Avatar>
+          <h2>singup</h2>
+          <Typography variant="caption">
+            Please fill this form to create an account
+          </Typography>
+        </Grid>
       </Paper>
     </Grid>
-    )
-    
-}
+  );
+};
 
-export default Signup
+export default Signup;
