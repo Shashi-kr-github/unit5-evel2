f6d8cbba0c42adc46b35a83cf62455cab50496fa
Shashijass <shashi16ic14@gmail.com>
2021-12-13 15:46:11 +0530

    2nd commit

---
+++ src/components/signup.js
@@ -0,0 +1,14 @@
+import {Grid, Paper} from '@mui/material/core'
+
+const Signup = () => {
+    return (
+    <Grid>
+      <Paper>
+        <h2>singup</h2>
+      </Paper>
+    </Grid>
+    )
+    
+}
+
+export default Signup
