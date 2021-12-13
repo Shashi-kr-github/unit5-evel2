fb8f08d29d473b029b022011d6ee660fe892f23d
Shashijass <shashi16ic14@gmail.com>
2021-12-13 16:24:25 +0530

    5th commit

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,17 +1,36 @@
-import { Avatar, Grid, Paper, Typography } from "@mui/material/";
+import {
+  Avatar,
+  Grid,
+  Paper,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material/";
+//import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 
 const Signup = () => {
   const paperStyle = { padding: "30px 20px", width: 300, margin: "20px auto" };
+  const headerStyle = { margin: 0 };
   return (
     <Grid>
       <Paper elevation={20} style={paperStyle}>
-        <Grid>
-          <Avatar></Avatar>
-          <h2>singup</h2>
+        <Grid align="center">
+          <Avatar>
+            {/* <AddCircleOutlineOutlinedIcon></AddCircleOutlineOutlinedIcon> */}
+          </Avatar>
+          <h2 style={headerStyle}>singup</h2>
           <Typography variant="caption">
             Please fill this form to create an account
           </Typography>
         </Grid>
+        <form>
+          <TextField fullWidth label="Name"></TextField>
+          <TextField fullWidth label="Email"></TextField>
+          <TextField fullWidth label="Phone-Number"></TextField>
+          <TextField fullWidth label="Password"></TextField>
+          <TextField fullWidth label="Confirm Password"></TextField>
+          <Button type="submit" variant="contained"></Button>
+        </form>
       </Paper>
     </Grid>
   );
